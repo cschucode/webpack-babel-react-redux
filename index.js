86ab1768dc363d3dbc1a92ca4bf5dffe83f1eb15
Chris Schuhmacher <chris@fandom.com>
2019-09-01 16:27:40 -0700

    Setting up the build

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react';
+
+import HelloWorld from './src/components/HelloWorld.jsx';
+
+ReactDOM.render(<HelloWorld />, document.getElementById('app'));
--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const HelloWorld = () => (
-  <div>Hello World</div>
+  <div>Hello World!</div>
 );
 
 export default HelloWorld;
