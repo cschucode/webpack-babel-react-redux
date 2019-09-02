ef6a3b1b3024f4cfc7a5c6fe8d7b8331f711da23
Chris Schuhmacher <chris@fandom.com>
2019-09-02 14:50:04 -0700

    Refactored the reducer

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,9 +1,18 @@
+import { ADD_ARTICLE } from '../constants/action-types';
+
 const initialState = {
   articles: []
 };
 
 function rootReducer(state = initialState, action) {
-  return state;
+  switch (action.type) {
+    case ADD_ARTICLE:
+      return Object.assign({}, state, {
+        articles: state.articles.concat(action.payload);
+      });
+    default:
+      return state;
+  }
 }
 
 export default rootReducer;
