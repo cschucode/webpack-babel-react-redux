fcee2e1e1316f436ca229fc23b6284898228ec52
Chris Schuhmacher <chris@fandom.com>
2019-09-02 16:12:05 -0700

    Added App and List component with redux state

--- index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import HelloWorld from './src/components/HelloWorld.jsx';
-
-ReactDOM.render(<HelloWorld />, document.getElementById('app'));
